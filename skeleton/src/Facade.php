f6c3c4af04336ad90df01edc759f980d2048197d
uyab <uyab.exe@gmail.com>
2015-09-10 16:32:09 +0700

    add facade file and folder lang/id

---
+++ skeleton/src/Facade.php
@@ -0,0 +1,13 @@
+<?php
+namespace :VendorName\:PackageName;
+
+class Facade extends \Illuminate\Support\Facades\Facade
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected static function getFacadeAccessor()
+    {
+        return ':package_name';
+    }
+}
